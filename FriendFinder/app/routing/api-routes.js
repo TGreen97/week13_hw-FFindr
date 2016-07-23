488e597139e2409c0b6adf5f414445f2410b6479
T. GREEN <toddgreen@utexas.edu>
2016-07-23 03:25:45 -0500

    170722_app updated - modal display issues

--- FriendFinder/app/routing/api-routes.js
+++ FriendFinder/app/routing/api-routes.js
@@ -1,64 +1,84 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+
 
 var friendData 		= require('../data/friends.js');
-//var waitListData 	= require('../data/waitinglist-data.js');
 var path 			= require('path');
 
 
-
-
-// ===============================================================================
+/*function compareScore(){
+	var userScore = 0;
+	for (var i = 0; i < friendData.length; i++) {
+			for (var j = 0; j < friendData[i].scores.length; j++) {
+				 userScore += friendData[i].scores[i];
+			}
+	}
+} */
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app){
 
-	// API GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases when a user visits a link
-	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-	// ---------------------------------------------------------------------------
-
 	app.get('/api/friends', function(req, res){
 		res.json(friendData);
 	});
 
-	// API POST Requests
-	// Below code handles when a user submits a form and thus submits data to the server.
-	// In each of the below cases, when a user submits form data (a JSON object)
-	// ...the JSON is pushed to the appropriate Javascript array
-	// (ex. User fills out a reservation request... this data is then sent to the server...
-	// Then the server saves the data to the tableData array)
-	// ---------------------------------------------------------------------------
-
 	app.post('/api/friends', function(req, res){
+		var bestMatch = {
+			name: "",
+			photo: "",
+			scoreDiff: 50
+		};
 
-		if(friendData.length < 5 ){
-			friendData.push(req.body);
-			res.json(true); // KEY LINE
-		}
+		var userData = req.body;
+		var userName = userData.name;
+		var userPhoto = userData.photo;
+		var userScores = userData.scores;
 
-		// Or false if they don't have a table
-		else{
-			waitListData.push(req.body);
-			res.json(false); // KEY LINE
-		}
+		var totalDiff = 0;
 
+		for (var i=0; i<friendData.length; i++){
+			console.log (friendData[i].name);
+
+			for (var j=0; j<friendData[i].scores[j]; j++){
+
+				totalDiff += Math.abs(userScores[j]- friendData[i].scores[j]);
+
+				if (totalDiff <= bestMatch.scoreDiff){
+
+					bestMatch.name = friendData[i].name;
+					bestMatch.image = friendData[i].image;
+					bestMatch.scoreDiff = totalDiff;
+        }
+			}
+}
+
+
+		console.log(bestMatch);
+		friendData.push(userData);
+		res.json(bestMatch);
+		//compareScore();
+		/* friendData.forEach(function(item) {
+				var currDiff = 0;
+				for (var i = 0; i < item.scores.length; i++) {
+					currDiff += Math.abs(item.scores[i] - req.body.scores[i]);
+				}
+				if (currDiff <= scoreDiff) {
+						scoreDiff = currDiff;
+						bestMatch = item;
+				}
+			}) */
+		 // KEY LINE
 	});
 
 	// ---------------------------------------------------------------------------
 	// I added this below code so you could clear out the table while working with the functionality.
 	// Don't worry about it!
 
-	/* app.post('/api/clear', function(req, res){
+	app.post('/api/clear', function(req, res){
 		// Empty out the arrays of data
-		tableData = [];
-		waitListData = [];
-
-		console.log(tableData);
-	}) */
+		friendData = [];
+		console.log(friendData);
+	})
 }
--- FriendFinder/app/routing/html-routes.js
+++ FriendFinder/app/routing/html-routes.js
@@ -3,21 +3,12 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 var path = require('path');
-
-
-
-
-
-// ===============================================================================
+var bodyParser = require('body-parser');
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app){
 
-	// HTML GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases the user is shown an HTML page of content
-	// ---------------------------------------------------------------------------
 
 	app.get('/survey', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
